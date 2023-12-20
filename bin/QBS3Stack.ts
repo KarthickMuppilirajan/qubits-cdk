6325f9903382b5ac8194362a240a2bc27a9738fe
Karthick Muppilirajan <veeramani.karthick@fourbends.com>
2023-12-20 16:09:00 +0530

    FIx: Added package.json file

--- bin/QBS3Stack.ts
+++ bin/QBS3Stack.ts
@@ -3,8 +3,8 @@ import * as cdk from 'aws-cdk-lib';
 import {QbS3Stack} from '../lib/QbS3Stack';
 
 const app = new cdk.App();
-new QbS3Stack(app, 'cdk-stack', {
-  stackName: 'cdk-stack',
+new QbS3Stack(app, 'QbS3Stack', {
+  stackName: 'QbS3Stack',
   env: {
     region: process.env.AWS_REGION,
   
